ad42a32a80bee07fde9cf1e6c1e866339a6e0d45
Chris Binggeli <99316773+GNUNotUsername@users.noreply.github.com>
2023-12-25 18:23:06 +1000

    Better timings

--- embed/MozAVRt.c
+++ embed/MozAVRt.c
@@ -1,6 +1,7 @@
 #include <stdint.h>
 
 #include <avr/io.h>
+#include <avr/interrupt.h>
 
 
 /*      DDR     */
@@ -9,8 +10,8 @@
 
 
 typedef struct Note {
-    uint64_t    start;
-    uint64_t    stop;
+    uint32_t    start;
+    uint32_t    stop;
     double      freq;
     struct Note *next;
 };
@@ -19,8 +20,14 @@ typedef struct Note {
 void init_hardware();
 
 
+volatile uint32_t time;
+
+
 int main (void) {
     init_hardware();
+    //receive_music();
+
+    while (1) ;
 
     return (0);
 }
@@ -32,8 +39,19 @@ void init_hardware() {
     DDRC    = DD_IN;
     DDRD    = DD_IN;
     TCCR1A  = 0;
-    TCCR1B  = (1 << WGM12) | (1 << CS11);
+    TCCR1B  = (1 << WGM12) | (1 << CS10);
     TCCR1C  = 0;
-    OCR1A   = 999;
+    OCR1A   = 0;
+    TCNT1   = 0;
+    TIMSK1  = (1 << OCIE1A);
+    TIFR1   |= (1 << OCF1A);
+    time    = 0;
+    sei();
     // TODO when usart is implemented, it will go here
+}
+
+
+ISR(TIMER1_COMPA_vect) {
+    time++;
+    PORTA = (time >> 8) & 0xFF;
 }
